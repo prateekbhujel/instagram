e1d5882f4e168e589a2a40e63a2b9d0cc3dad9b3
Pratik Bhujel <prateekbhujelpb@gmail.com>
2024-05-02 18:01:52 +0545

    #[2.10]--'Added Mobile Headers for notification and search and removed borders....'

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,15 +1,48 @@
-<div class="w-full">
+<div class="w-full h-full">
 
     {{-- Header Section Start --}}
-    <header>
+    <header class="md:hidden sticky top-0 bg-white">
 
+        <div class="grid grid-cols-12 gap-2 items-center">
+  
+            <div class="col-span-3">
+  
+              <img src="{{asset('public/assets/logo.png')}}"  class="h-12 max-w-lg w-full" alt="logo">
+  
+            </div>
+  
+              <div class="col-span-8 flex justify-center px-2">
+  
+                <input type="text" placeholder="Search" 
+                 class=" border-0 outline-none w-full focus:outline-none bg-gray-100 rounded-lg focus:ring-0 hover:ring-0" 
+                >
+                
+              </div>
+  
+            <div class="col-span-1 flex justify-center">
+  
+              <a href="#">
+                <span>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.9" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                  </svg>
+                  
+                </span>
+              </a>
+              
+            </div>
+  
+  
+        </div>
+  
+  
     </header>
     {{-- Header Section End --}}
 
     {{-- Main Section Start --}}
     <main class="grid lg:grid-cols-12 gap-8 md:mt-10">
         
-        <aside class="lg:col-span-8 border overflow-hidden h-[1000px]">
+        <aside class="lg:col-span-8 overflow-hidden">
              
             {{-- Story section Start --}}
             <section>
@@ -34,11 +67,13 @@
 
             </section>
             {{-- Story section End --}}
+
+
         </aside>
         
 
         {{-- Suggestion and applinks Section Start --}}
-        <aside class="lg:col-span-4 border hidden lg:block p-4">
+        <aside class="lg:col-span-4 hidden lg:block p-4">
 
             <div class="flex items-center gap-2">
 
@@ -53,24 +88,26 @@
                 <h4 class="fomt-bold text-gray-700/95">Suggestions for you </h4>
                 
                 <ul class="my-2 space-y-3">
-                    <li class="flex items-center gap-3">
-                        <x-avatar src="https://source.unsplash.com/500x500?face-3" class="w-12 h-12" />
+                    @for($i = 0; $i < 5; $i++)
+                        <li class="flex items-center gap-3">
+                            <x-avatar src="https://source.unsplash.com/500x500?face-{{ $i }}" class="w-12 h-12" />
 
-                        <div class="grid grid-cols-7 w-full gap-2">
-                            <div class="col-span-5">
-                                <h5 class="font-semibold truncate text-sm">{{ fake()->name }}</h5>
-                                <p class="text-xs truncate"> Followed by {{ fake()->name }}</p>
+                            <div class="grid grid-cols-7 w-full gap-2">
+                                <div class="col-span-5">
+                                    <h5 class="font-semibold truncate text-sm">{{ fake()->name }}</h5>
+                                    <p class="text-xs truncate"> Followed by {{ fake()->name }}</p>
+                                </div>
+
+                                <div class="col-span-2 flex text-right justify-end">
+
+                                    <button class="font-bold text-blue-500/100 ml-auto text-sm">Follow</button>
+
+                                </div>
+
                             </div>
-
-                            <div class="col-span-2 flex text-right justify-end">
-
-                                <button class="font-bold text-blue-500/100 ml-auto text-sm">Follow</button>
-
-                            </div>
-
-                        </div>
-                        
-                    </li>
+                            
+                        </li>
+                    @endfor
                 </ul>
 
             </section>
@@ -89,7 +126,7 @@
                     <li class="text-xs text-gray-800/95 font-medium"><a href="#" class="hover:underline">Location</a></li>
 
                 </ol>
-                <h3 class="text-gray-800/92 mt-6 text-sm">© 2024 INSTAGRAM FROM YOU BY <a href="https://instagram.com/prateekbhujel" class="hover:text-blue-500">PRATIK BHUJEL</a></h3>
+                <h3 class="text-gray-800/92 mt-6 text-sm">© 2024 INSTAGRAM FROM YOU BY <a href="https://instagram.com/prateekbhujel" class="hover:text-blue-500" target="_blank">PRATIK BHUJEL</a></h3>
 
             </section>
             {{-- Applinks Section End --}}
