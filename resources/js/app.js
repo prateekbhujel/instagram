c9bb5225a03471f6de1c0ffbe7a057891d8fe92f
Pratik Bhujel <prateekbhujelpb@gmail.com>
2024-05-03 13:21:28 +0545

    #[14.]--'Swiper Interactivity added with custom build navigation bar...'

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,3 +12,14 @@ import intersect from '@alpinejs/intersect'
 Alpine.plugin(intersect)
 Alpine.start();
 
+// core version + navigation, pagination modules:
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+// import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+window.Swiper=Swiper;
+window.Navigation=Navigation;
+window.Pagination=Pagination;
