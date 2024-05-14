cc2be7e01e796ac90419d7c1749396431153598a
Pratik Bhujel <prateekbhujelpb@gmail.com>
2024-05-14 14:27:42 +0545

    #[36.] 'Installing an Like Package i.e. Overtrue Laravel Package '

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -7,11 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Overtrue\LaravelLike\Traits\Likeable;
+
 
 class Comment extends Model
 {
-    use HasFactory;
-    use SoftDeletes;
+    use HasFactory, SoftDeletes, Likeable;
 
     protected $guarded = [];
 
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,10 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Overtrue\LaravelLike\Traits\Likeable;
+use Overtrue\LaravelLike\Traits\Liker;
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory, Liker, Likeable;
 
     protected $guarded = [];
 
