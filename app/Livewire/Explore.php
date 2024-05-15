454b7ddefbd1438233b0a042ab740ec48b0a6179
Pratik Bhujel <prateekbhujelpb@gmail.com>
2024-05-15 14:27:11 +0545

    #[50.]--'Created an Explore Page desgind and Fetehced data from the database.'

--- app/Livewire/Explore.php
+++ app/Livewire/Explore.php
@@ -2,12 +2,31 @@
 
 namespace App\Livewire;
 
+use App\Models\Post;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class Explore extends Component
 {
+    public $var;
+
+
+
+    #[On('closeModal')]
+    public function revertUrl()
+    {
+        $this->js("history.replaceState({}, '', '/explore')");
+
+    }//End Method
+
+
     public function render()
     {
-        return view('livewire.explore');
-    }
+        $posts = Post::limit(20)->get();
+
+        return view('livewire.explore', compact('posts'));
+
+    }//End Method
+
+
 }
