86297fbf5b5fb9124cd093cb5776ea24c383dc6d
Pratik Bhujel <prateekbhujelpb@gmail.com>
2024-05-22 19:31:05 +0545

    #[74.]--'Created an Converstation In Backend logic made and written.'

--- app/Livewire/Profile/Home.php
+++ app/Livewire/Profile/Home.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Profile;
 
+use App\Models\Conversation;
 use App\Models\User;
 use App\Notifications\NewFollowerNotification;
 use Livewire\Attributes\On;
@@ -31,6 +32,37 @@ class Home extends Component
             $this->user->notify(new NewFollowerNotification(auth()->user()));
         
          
+
+    }//End Method
+
+
+    public function message($userId)
+    {
+        $authenticatedUserId = auth()->id();
+        
+        //check if the user is not the authenticated user
+        $existingCoversation = Conversation::where(function($query) use($authenticatedUserId, $userId){
+            $query->where('sender_id', $authenticatedUserId)
+                  ->where('receiver_id', $userId);
+
+        })->orWhere(function($query) use($authenticatedUserId, $userId){
+            $query->where('sender_id', $userId)
+                  ->where('receiver_id', $authenticatedUserId);
+        })->first();
+
+        if($existingCoversation) 
+        {
+            //redirect to the conversation
+            return redirect()->route('chat.main', ['chat' => $existingCoversation->id]);
+        }
+
+        //Create new conversation
+        $createdConversation = Conversation::create([
+           'sender_id' => $authenticatedUserId,
+           'receiver_id' => $userId,
+        ]);
+        
+        return redirect()->route('chat.main', ['chat' => $createdConversation->id]);
 
     }//End Method
 
